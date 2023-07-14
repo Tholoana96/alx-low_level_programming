734baac62643f471aa2881df4cb25075bb486be5
Tholoana <tholoana.manyane@gmail.com>
2023-07-14 04:46:48 -0700

    Possible number combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Function used to display all possible number combinations
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 10; i < 20; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i != 19)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
