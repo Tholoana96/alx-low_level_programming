ce2b348ab6ac462d5b135a19a0f4a6bc8304d321
Tholoana <tholoana.manyane@gmail.com>
2023-07-14 04:10:11 -0700

    Lowercase alphabets ecept q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,13 +12,8 @@ int main(void)
 	char a;
 
 	for (a = 'a'; a <= 'z' ; a++)
-	{
-		if (a == 'q' || a == 'e')
-		{
-			continue;
-		}
-		putchar(a);
-	}
+		if (a != 'q' &&  a != 'e')
+			putchar(a);
 	putchar('\n');
 	return (0);
 }
