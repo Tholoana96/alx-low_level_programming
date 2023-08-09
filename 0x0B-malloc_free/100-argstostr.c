31f2f972b0081216d8d6af33274831ea97151d28
Tholoana <tholoana.manyane@gmail.com>
2023-08-09 03:27:49 -0700

    A function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,46 +2,48 @@
 #include <stdlib.h>
 
 /**
- * argstostr - gives array of args
- * @ac: argument count
- * @av: array of arguments
- * Return: char value
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: arguments count
+ * @av: arguments vector
+ *
+ * Return: a pointer to a new string, or NULL if it fails
  */
-
 char *argstostr(int ac, char **av)
 {
-	int size;
-	char *s;
-	int i;
-	int j;
-	int k;
+	char *str, *s;
+	int i, j, k, len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			size++;
+		s = av[i];
+		j = 0;
+
+		while (s[j++])
+			len++;
+		len++;
 	}
 
-	size += (ac + 1);
-	s = malloc(sizeof(char) * size);
-	if (s == NULL)
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
 		return (NULL);
-	k = 0;
-	for (i = 0; i < ac; i++)
+
+	for (i = 0, j = 0; i < ac && j < len; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		s = av[i];
+		k = 0;
+
+		while (s[k])
 		{
-			s[k] = av[i][j];
+			str[j] = s[k];
 			k++;
+			j++;
 		}
-		s[k] = '\n';
-		k++;
+		str[j++] = '\n';
 	}
+	str[j] = '\0';
 
-	s[k] = '\0';
-
-	return (s);
+	return (str);
 }
